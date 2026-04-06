842a5f7fb8492647f94ca1f231d168245dcd6456
miksmiko-dev <m.miko.rosales@gmail.com>
2026-04-06 10:57:13 +0800

    [FEAT] - added cors

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,20 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new AllExeptionsFilter());
+  app.enableCors({
+    origin: (
+      origin: string | undefined,
+      callback: (err: Error | null, allow?: boolean) => void,
+    ) => {
+      const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000'];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
